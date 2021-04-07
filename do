ffdb2ceb54e45beec2564cee6ad71cf6a3b97388
peterpaullake <peterpaullake@gmail.com>
2021-04-07 19:52:44 -0300

    messing around with traffic stuff

---
+++ do
@@ -0,0 +1,6 @@
+Make a very bad version of the presentation along with a script.txt
+
+The main strokes are:
+  - steady states
+  - script draft
+  - demo with various parameters to demonstrate different regimes
--- swarm.lisp
+++ swarm.lisp
@@ -126,36 +126,19 @@
 
     ;; MODEL
 
-    (defun compute-pred-dxdt (pred-xy prey-coords &optional (c *c*) (p *p*))
-      (let ((n (length prey-coords))
-	    (z pred-xy))
-	(scale-vec (/ c n)
-		   (do-vec-sum k n
-		     (let ((xk (elt prey-coords k)))
-		       (scale-vec (/ 1 (expt (vec-len2 (vec- xk z)) (/ p 2)))
-				  (vec- xk z)))))))
-
-    (defun compute-prey-dxdts (pred-xy prey-coords &optional (a *a*) (b *b*))
-      (let ((n (length prey-coords)))
-	(labels ((compute-prey-dxdt (j)
-		   (let ((z pred-xy)
-			 (xj (elt prey-coords j)))
-		     (vec+ (scale-vec (/ 1 n)
-				      (do-vec-sum k n
-					(if (= k j)
-					    '(0 0)
-					    (let ((xk (elt prey-coords k)))
-					      (vec- (scale-vec (/ 1 (vec-len2
-								     (vec- xj
-									   xk)))
-							       (vec- xj xk))
-						    (scale-vec a
-							       (vec- xj xk)))))))
-			   (scale-vec (/ b (vec-len2 (vec- xj z)))
-				      (vec- xj z))))))
-	  (map-index #'compute-prey-dxdt (length prey-coords)))))
+    (defun compute-car-dxdts (coords)
+      HERE)
 
     ;; CANVAS
+
+    (defun fit-canvas-to-container (canvas &optional height)
+      (setf (chain canvas style width) "100%")
+      (setf (chain canvas width) (chain canvas offset-width))
+      (if (eq height undefined)
+	  (progn
+	    (setf (chain canvas style height) "100%")
+	    (setf (chain canvas height) (chain canvas offset-height)))
+	  (setf (chain canvas height) height)))
 
     (defun color->str (color)
       "Convert a 3d color with components
@@ -256,57 +239,21 @@
 			  " has an unrecognized type: "
 			  ',type)))))
 
+    (def-param (*paused* "Paused" :checkbox :var value :value t))
+
     (def-param (*reset-sim* "Reset simulation" :link)
 	(init-coords *game-state*))
-
-    (def-param (*lock-view-to-pred* "Lock view to predator" :link)
-	(setf (chain *game-state* lock-view-to-pred?) t)
-      (setf (chain *game-state* lock-view-to-prey?) f))
-
-    (def-param (*lock-view-to-pred* "Lock view to prey" :link)
-	(setf (chain *game-state* lock-view-to-pred?) f)
-      (setf (chain *game-state* lock-view-to-prey?) t))
-
-    (def-param (*unlock-view* "Unlock view" :link)
-	(setf (chain *game-state* lock-view-to-pred?) f)
-      (setf (chain *game-state* lock-view-to-prey?) f))
 
     (def-param (*sim-speed* "Simulation speed" :range :var value
 						      :value 0.001
 						      :min 0.0001
 						      :max 0.0012))
 
-    (def-param (*n-prey* "N (number of prey)" :range :var value
-						     :value 40
+    (def-param (*n-cars* "N (number of cars)" :range :var value
+						     :value 6
 						     :min 1
 						     :max 500)
 	(init-coords *game-state*))
-
-    (def-param (*a* "a (swarm->swarm attraction)" :range :var value
-							 :value 0.796
-							 :min 0
-							 :max 10))
-
-
-    (def-param (*b* "b (prey->predator repulsion)" :range :var value
-							  :value 0.708
-							  :min 0
-							  :max 10))
-
-    (def-param (*c* "c (predator->prey attraction)" :range :var value
-							   :value 15
-							   :min 0
-							   :max 20))
-
-    (def-param (*p* "p (predator->prey attraction falloff)" :range :var value
-								   :value 2.1985
-								   :min 0
-								   :max 5))
-
-    (def-param (*show-help* "Show help" :checkbox :var value :value f)
-	(let ((help-table ((chain document get-element-by-id) "help-table")))
-	  (setf (chain help-table style visibility)
-		(if value "visible" "hidden"))))
     
     ;; GAME
 
@@ -378,21 +325,28 @@
 	  (push (list (random) (random))
 		prey-coords))))
 
-    (defun init-coords (state &optional (n-prey (round *n-prey*)))
-      (setf (getprop state 'pred-xy)
-	    (list (random) (random)))
-      (setf (getprop state 'prey-coords)
-	    (make-prey-coords n-prey)))
+    (defun init-coords (state &optional (n-cars (round *n-cars*)))
+      ;; Initialize the cars in a line with no speed.
+      (setf (chain state coords) (list))
+      (dotimes (i (* n-cars))
+	(push (* 0.5 i) (chain state coords))
+	(push 0 (chain state coords))))
 
-    (defun init-game (state canvas &optional (width 800) (height 400))
-      (setf (getprop state 'canvas) canvas)
-      (setf (chain canvas width) width)
-      (setf (chain canvas height) height)
-      (setf (getprop state 'view-width) 8)
-      (setf (getprop state 'view-xy) (list 0.5 0.5))
-      (setf (chain state lock-view-to-pred?) f)
-      (setf (chain state lock-view-to-prey?) f)
+    (defun init-game (state canvas &optional (height 300))
+      (setf (chain state canvas) canvas)
+      (fit-canvas-to-container canvas height)
+      (setf (chain state view-width) 8)
+      (setf (chain state view-xy) (list 0.5 0.5))
       (init-coords state)
+
+      ((chain document add-event-listener) "keydown"
+       (lambda (e)
+	 (cond ((equal (chain e code) "KeyH")
+		(msg "Left")
+		(setf (chain *game-state* left?) t))
+	       ((equal (chain e code) "KeyL")
+		(msg "Right")
+		(setf (chain *game-state* right?) t)))))
 
       ((chain canvas add-event-listener) "mousedown"
        (lambda (e)
@@ -406,24 +360,6 @@
       ((chain canvas add-event-listener) "mouseup"
        (lambda (e)
 	 (delete (getprop state 'last-click-pos))))
-
-      ((chain document add-event-listener) "keydown"
-       (lambda (e)
-	 (unless (eq (chain state mouse-xy) undefined)
-	   (cond ((equal (chain e code) "ControlLeft")
-		  (unless (chain state lock-prey?)
-		    (setf (chain state lock-pred?) t)))
-		 ((equal (chain e code) "ShiftLeft")
-		  (unless (chain state lock-pred?)
-		    (setf (chain state lock-prey?) t)))))))
-
-      ((chain document add-event-listener) "keyup"
-       (lambda (e)
-	 (unless (eq (chain state mouse-xy) undefined)
-	   (cond ((equal (chain e code) "ControlLeft")
-		  (setf (chain state lock-pred?) f))
-		 ((equal (chain e code) "ShiftLeft")
-		  (setf (chain state lock-prey?) f))))))
 
       ((chain canvas add-event-listener) "mousemove"
        (lambda (e)
@@ -457,52 +393,28 @@
 	     (incf (chain state view-width) (* 0.01 (chain e delta-y)))))))
 
     (defun step-game (state dt)
-      (let* ((speed *sim-speed*)
-	     (pred-xy (getprop state 'pred-xy))
-	     (prey-coords (getprop state 'prey-coords))
-	     (prey-dxdts (compute-prey-dxdts pred-xy prey-coords)))
-	;; Update the predator position
-	(if (chain state lock-pred?)
-	    (setf (chain state pred-xy)
-		  (canvas-xy->game-xy state (chain state mouse-xy)))
-	    (vec-incf (getprop state 'pred-xy)
-		      (scale-vec (* speed dt)
-				 (compute-pred-dxdt pred-xy prey-coords))))
-	;; Update the prey positions
-	(dotimes (i (length prey-coords))
-	  (if (and (= i 0) (chain state lock-prey?))
-	      (setf (elt prey-coords i)
-		    (canvas-xy->game-xy state (chain state mouse-xy)))
-	      (vec-incf (elt prey-coords i)
-			(scale-vec (* speed dt) (elt prey-dxdts i)))))
-	;; If the view is locked, update the view position
-	(cond ((chain state lock-view-to-pred?)
-	       (setf (chain state view-xy)
-		     (setf (chain state view-xy)
-			   (chain state pred-xy))))
-	      ((and (chain state lock-view-to-prey?)
-		    (>= (length (chain state prey-coords)) 1))
-	       (setf (chain state view-xy)
-		     (setf (chain state view-xy)
-			   (elt (chain state prey-coords) 0)))))))
+      (unless *paused*
+	(let ((speed *sim-speed*)
+	      (coords (chain state coords)))
+	  ;; Update the car positions and velocities
+	  nil)))
 
     (defun draw-game (state)
       (with-game-state state
 	(clear-canvas ctx)
 	(draw-grid state)
-	(draw-circle state (getprop state 'pred-xy) '(1 0 0))
-	(dotimes (i (length (chain state prey-coords)))
-	  ;; If the view is locked to the prey, draw the first prey in blue
-	  (if (and (= i 0) (chain state lock-view-to-prey?))
-	      (draw-circle state (elt (chain state prey-coords) i) '(0 0 1))
-	      (draw-circle state (elt (chain state prey-coords) i))))))
+	(let ((n-cars (/ (length (chain state coords)) 2)))
+	  (dotimes (i n-cars)
+	    (let ((pos (elt (chain state coords) (* 2 i))))
+	      (draw-circle state (list pos 0)
+			   (if (= i (1- n-cars)) '(1 0 0) '(0 1 0))))))))
 
     (defun run-game (state)
       (labels ((do-frame ()
-		 (let ((dt (if (eq (getprop state 'prev-time) undefined)
+		 (let ((dt (if (eq (chain state prev-time) undefined)
 			       0
 			       (- (get-time) (getprop state 'prev-time)))))
-		   (setf (getprop state 'prev-time) (get-time))
+		   (setf (chain state prev-time) (get-time))
 		   (step-game state dt)
 		   (draw-game state)
 		   ((chain window request-animation-frame) #'do-frame))))
@@ -510,7 +422,7 @@
 
     (defparameter *game-state* (create))
     (init-game *game-state* ((chain document get-element-by-id) "game-canvas"))
-    ;; (run-game *game-state*)
+    (run-game *game-state*)
 
     ;; SLIDES
 
@@ -550,50 +462,8 @@
 	  ((chain -math-jax typeset)))))
 
     (defparameter *slide-i* 0)
-    (defparameter *slides*
-      (list
-       (make-slide "Predator-swarm interactions" '())
-       (make-slide "The paper"
-		   '("A minimal model of predator-swarm interactions"
-		     "Chen and Kolokolnikov"))
-       (make-slide "Motivation"
-		   '("Group of prey being hunted by a predator"
-		     "Zebras vs lion, etc"
-		     "The prey swarm while trying to get away."))
-       (make-slide "The model"
-		   (list (+ "Let \\(\\mathbf{x}_j(t) \\in \\mathbb{R}^2 \\) "
-			    "be the position of the "
-			    "\\(j^{\\text{th}}\\) "
-			    "prey at time \\(t\\).")
-			 (+ "Let \\(\\mathbf{z}(t) \\in \\mathbb{R}^2 \\) "
-			    "be the position of the "
-			    "predator at time \\(t\\).")
-			 (+ "Assume the prey and predator "
-			    "obey Newton's second law: "
-			    "\\(\\mathbf{F} = m \\mathbf{a}\\), "
-			    "and that the prey and predator "
-			    "have friction with the ground.")
-			 (+ "\\(m_j \\frac{d^2 \\mathbf{x}_j}{dt^2} "
-			    " = \\mathbf{F}_{\\text{prey$\\to$prey}_j} "
-			    " + \\mathbf{F}_{\\text{predator$\\to$prey}_j} "
-			    " - \\mu \\frac{d \\mathbf{x}_j}{\dt} \\),")))
-       (make-slide "???"
-		   '("\\(N\\) prey live on \\(R^2\\)"
-		     "One predator lives on \\(R^2\\)"
-		     "The predator is trying to catch the prey"))
-       ))
-    (show-slide 0 *slides*)
-
-    ((chain document add-event-listener) "keydown"
-     (lambda (e)
-       (cond ((equal (chain e code) "KeyJ")
-	      (when (< *slide-i* (1- (length *slides*)))
-		(incf *slide-i*)
-		(show-slide *slide-i* *slides*)))
-	     ((equal (chain e code) "KeyK")
-	      (when (> *slide-i* 0)
-		(decf *slide-i*)
-		(show-slide *slide-i* *slides*))))))))
+    (defparameter *slides* (list (make-slide "..." '("..."))))
+    (show-slide *slide-i* *slides*)))
 
 (defparameter *html*
   (with-html-output-to-string (s nil :indent t)
@@ -602,50 +472,16 @@
       (:meta :charset "utf-8")
       (:title "Predator-swarm")
       (str (uiop:read-file-string "fonts.html"))
-      (:style (str (uiop:read-file-string "style.css")))
-      (str (uiop:read-file-string "mathjax.html")))
-     ;; (:link :href "style.css" :rel "stylesheet"))
+      (:style (str (uiop:read-file-string "style.css"))))
+     ;; (str (uiop:read-file-string "mathjax.html")))
      (:body :class "dark-bg"
-	    ;; (:h2 :class "text"
-	    ;; 	 "A minimal model of predator-swarm interactions")
-	    (:a :class "text"
-		:href "https://arxiv.org/abs/1403.3250"
-		"Paper")
-	    (:br)
-	    (:a :class "text"
-		:href "https://github.com/peterpaullake/swarm"
-		"GitHub repo")
-	    (:br)
-	    (:br)
-	    ;; (:a :class "text"
-	    ;; 	:href "javascript:;"
-	    ;; 	"Live demo")
 	    (:div :class "row light-bg"
 		  (:div :id "slides-div" :class "column")
 		  (:div :id "media-div" :class "column"
-			(:img :src "swarm-01.png")
-			(:table :id "params-table"
-				:style "visibility: hidden;")))
-	    (:canvas :id "game-canvas")
-	    (:table :id "help-table" :style "visibility: hidden;"
-		    (:tr
-		     (:td (:h2 :class "text" "Help"))
-		     (:td))
-		    (:tr
-		     (:td (:p :class "text" "Control predator"))
-		     (:td (:p :class "text" "Hold ctrl")))
-		    (:tr
-		     (:td (:p :class "text" "Control prey"))
-		     (:td (:p :class "text" "Hold shift")))
-		    (:tr
-		     (:td (:p :class "text" "Zoom in/out"))
-		     (:td (:p :class "text" "Scroll")))
-		    (:tr
-		     (:td (:p :class "text" "Move viewport"))
-		     (:td (:p :class "text" "Click and drag")))))
+			(:canvas :id "game-canvas")
+			(:table :id "params-table"))))
      (:script :type "text/javascript" 
 	      (str (ps (lisp *ps-lisp-library*))))
-     ;; (:script :src "solver.js")
      (:script :type "text/javascript" (str *js*)))))
 
 (defun save-str-to-file (str path)
@@ -657,13 +493,6 @@
 
 (define-easy-handler (home :uri "/") () *html*)
 
-;; (define-easy-handler (solver-js :uri "/solver.js") ()
-;;   (uiop:read-file-string "solver/solver.js"))
-
-;; (define-easy-handler (solver-wasm :uri "/solver.wasm") ()
-;;   (setf (content-type*) "application/wasm")
-;;   (uiop:read-file-string "solver/solver.wasm"))
-
 (defparameter *server*
   (start (make-instance 'easy-acceptor
 			:address "0.0.0.0" ;; localhost
@@ -671,28 +500,8 @@
 			:port 8080)))
 
 "
-introduce the model the same way the paper does (f = ma)
-fix mathjax blocking issue (maybe wait until it's loaded before
-  drawing a slide?)
-try wasm stuff
-add a way to pause/resume
-You should have float ranges and int ranges
-allow for multiple predators
-You shouldn't be able to control the prey while the
-  view is locked to the prey. Similarly for predator.
-add a toggle for the lock view
-add a play/pause button
-add presets to look at the various regimes
-make canvas resizable
-pressing ctrl and shift with the cursor
-  outside the canvas shouldn't do anything
-you should be able to turn on trails
-make this into a multiplayer game (like
-  a lame version of slither.io)
-When you define a user param, you should have to specify a default value,
-  and there should be a 'reset params and view' button.
-Try the second-order model
-Try swapping predator/prey roles. Basically a simulator for the game of tag.
-Try having the predator actually eat the prey, and plot the number
-  of prey over time like in the paper
+write code for the model that just steps
+  the lead car according to the arrow keys
+write code for the model (continue writing from HERE)
+step the cars
 "
